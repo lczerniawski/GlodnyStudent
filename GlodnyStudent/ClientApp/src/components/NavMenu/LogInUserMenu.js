3bdccff1686d1cd16ed62ccff3d26118433d0568
tHeather <wrzosek46@gmail.com>
2019-06-08 23:01:57 +0200

    Add reports list  on admin panel

--- GlodnyStudent/ClientApp/src/components/NavMenu/LogInUserMenu.js
+++ GlodnyStudent/ClientApp/src/components/NavMenu/LogInUserMenu.js
@@ -8,7 +8,7 @@ export default class LogInUserMenu extends Component {
             <div>
                 <p>{sessionStorage.getItem("username")}</p>         
                 <ul className="navList">
-                    <li onClick={(e)=>this.props.toggleAdminPanel(e)} >Lista użytkowników</li> 
+                    <li onClick={(e)=>this.props.toggleAdminPanel(e)} >Panel Administracyjny</li> 
                     <li><Link to="/DodajRestauracje" >Zgłoś restaurację</Link></li> 
                     <li><Link to="/" >Zmień hasło</Link></li>  
                     <li onClick={(e) =>this.props.handleLogOut(e)} >Wyloguj</li>
