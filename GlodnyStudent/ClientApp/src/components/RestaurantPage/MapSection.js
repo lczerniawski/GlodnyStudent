c71f9ba43628e73f6b4fcc4627c6fec487e5220a
larenen <luki.lary@gmail.com>
2019-06-11 19:49:54 +0200

    Map

--- GlodnyStudent/ClientApp/src/components/RestaurantPage/MapSection.js
+++ GlodnyStudent/ClientApp/src/components/RestaurantPage/MapSection.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import Map from 'pigeon-maps';
 import Marker from 'pigeon-marker';
 import './MapSection.css';
+import Geocode from "react-geocode";
+ 
+// set Google Maps Geocoding API for purposes of quota management. Its optional but recommended.
+Geocode.setApiKey("AIzaSyBxvJXLoj0DtoGczKojLEo_Kc3LsdlPxCQ ");
+ 
+// Enable or disable logs. Its optional.
+Geocode.enableDebug();
 
 export default class MapSection extends Component {
 
@@ -17,13 +24,45 @@ export default class MapSection extends Component {
             streetNumberValidResult:false,
             localNumberValidResult:false,
             streetNumberErrorMessage:"",
-            zoom: 17
+            zoom: 17,
+            locationY:null,
+            locationX:null,
+         
+
+            streetNameErrorMessage:"",
+            localNumberErrorMessage:"",
+            restaurantNameErrorMessage:"",
         }
         
        /*  this.handleInputChange = this.handleInputChange.bind(this); */
         this.inputValidate = this.inputValidate.bind(this);
         this.makeAddresObject = this.makeAddresObject.bind(this);
+        this.getCoords = this.getCoords.bind(this);
     }
+
+
+    getCoords(streetName,streetNumber){
+        const address =`${streetName} ${streetNumber}`
+        Geocode.fromAddress(address).then(
+            response => {
+                console.log(response.results[0].geometry.location);
+                this.setState({
+     
+                    locationY: response.results[0].geometry.location.lat,
+                    locationX: response.results[0].geometry.location.lng,        
+                    mapResonseMessage:null
+                });
+            },
+            error => {
+                console.error(error);
+                this.setState({
+                    mapResonseMessage:"Ulica nie została znaleziona."
+                }); 
+            }
+        );
+    }
+
+
 
     zoomIn = () => {
       this.setState({
@@ -50,8 +89,8 @@ export default class MapSection extends Component {
 
     
       makeAddresObject(){
-        const {streetName,streetNumber,localNumber,district} = this.state;
-       return {streetName,streetNumber,localNumber,district};
+        const {streetName,streetNumber,localNumber,district,locationY,locationX} = this.state;
+       return {streetName,streetNumber,localNumber,district,locationY,locationX};
       }
 
 
@@ -59,7 +98,8 @@ export default class MapSection extends Component {
       const value = event.target.value;
       const name = event.target.name;
       const nameValid = name + "ValidResult";
-      const {streetNumberValidResult,streetNameValidResult,localNumberValidResult} = this.state;
+      const errMsg = name + "ErrorMessage";
+      const {streetNumberValidResult,streetNameValidResult,localNumberValidResult,restaurantNameValidResult} = this.state;
       let validResult =true;
       let eventValid = false;
       const regNumbersAndOneLetterAtEnd = /^\d+[a-zA-Z]?$/;
@@ -70,33 +110,44 @@ export default class MapSection extends Component {
         validResult =false;
       }else{
         switch(event.target.name) {
+           case "restaurantName":
+                eventValid= (event.target.value.length > 2)?true:false;   
+               validResult = eventValid && streetNumberValidResult && localNumberValidResult && streetNameValidResult;
+            break;
+         /*  case "cuisine":
+              eventValid= true;
+              validResult = localNumberValidResult && streetNumberValidResult && streetNameValidResult && restaurantNameValidResult;
+            break;  */  
           case "streetName":
-              eventValid= (event.target.value.length > 2)?true:false;   
-               validResult = eventValid && streetNumberValidResult && localNumberValidResult;
+              this.getCoords(event.target.value,this.state.streetNumber);  
+              eventValid= this.state.mapResonseMessage?false:true;
+               validResult = eventValid && streetNumberValidResult && localNumberValidResult && restaurantNameValidResult;;
             break;
           case "streetNumber":
-              eventValid = regNumbersAndOneLetterAtEnd.test(String(event.target.value));
-              validResult = eventValid && streetNameValidResult && localNumberValidResult;
-              errorMessage  = (eventValid  )?"":"Numer ulicy musi składać się z ciągu samych cyfr, ewentulalnie zakończonego jedną literą";            
+              this.getCoords(this.state.streetName,event.target.value);
+              let crdres = this.state.mapResonseMessage?false:true;
+              eventValid = regNumbersAndOneLetterAtEnd.test(String(event.target.value)) && crdres;
+              validResult = eventValid && streetNameValidResult && localNumberValidResult && restaurantNameValidResult;
+              errorMessage  = (regNumbersAndOneLetterAtEnd.test(String(event.target.value)) )?null:"Numer ulicy musi składać się z ciągu samych cyfr, ewentulalnie zakończonego jedną literą";           
             break;
           case "localNumber":
-              eventValid = value;
-              validResult = eventValid && streetNumberValidResult && streetNameValidResult;           
+              eventValid= (event.target.value.length > 0)?true:false;
+              validResult = eventValid && streetNumberValidResult && streetNameValidResult && restaurantNameValidResult;           
             break;
-          case "district":
+          /* case "district":
               eventValid= true;
-              validResult = localNumberValidResult && streetNumberValidResult && streetNameValidResult;           
-            break;       
+              validResult = localNumberValidResult && streetNumberValidResult && streetNameValidResult && restaurantNameValidResult;           
+            break;  */      
           default:
         }
       }
-     
+     // dopisac wersje z krs
 
       this.setState({
         disabledSubmit: !validResult,
         [name]: value,
         [nameValid]:eventValid,
-        streetNumberErrorMessage:errorMessage
+        [errMsg]:errorMessage
       });
 
     }
@@ -111,6 +162,9 @@ export default class MapSection extends Component {
 
         const editInputs = this.props.ownerLogIn&&sessionStorage.getItem("id")?
         <form onSubmit={(e)=>this.props.updateAddress(e,this.makeAddresObject(),`${this.props.restaurantId}/UpdateAddress`)} id="addressInfo">
+          {this.state.streetNameErrorMessage}
+          {this.state.localNumberErrorMessage}
+          {this.state.restaurantNameErrorMessage}
         <div className="label-form">
             <label htmlFor="streetName">Ulica</label> 
             <input id="streetName" className="inputStyle" type="text" name="streetName" onChange={this.inputValidate} />
